b9857a81bf89bf949c2e30431fdc9baf6ff8925c
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-09-06 12:28:53 -0600

    added functions for weather div, search history, event listener, and city div.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,6 @@ var now = moment();
 
 var currentDay = (now.format("ll"));
 
-function createWeatherDiv() {
-    var weather = document.getElementById("weatherForecast")
-    var weatherDiv = document.createElement("div");
-    weatherDiv.className = "weatherDiv";
-    weather.append(weatherDiv);
-}
-
 var searchId = document.getElementById("Search")
 var searchBtn = document.createElement("button");
 searchBtn.className = "button btn btn-secondary";
@@ -25,4 +18,42 @@ searchId.append(searchBtn);
 var searchHistoryList = document.getElementById("cities");
 var searchHistory = JSON.parse(localStorage.getItem("cities")) || []
 var currentSearch = document.getElementById("currentSearch");
+
+var createWeatherDiv = function() {
+    var weather = document.getElementById("weatherForecast")
+    var weatherDiv = document.createElement("div");
+    weatherDiv.className = "weatherDiv";
+    weather.append(weatherDiv);
+}
+
+var renderSearchHistory = function() {
+    searchHistoryList.innerHTML = "";
+    for (let citiesSearched = 0; citiesSearched < searchHistory.length; citiesSearched++) {
+        addCityDiv(searchHistory[citiesSearched])
+    }
+}
+
+var addCityDiv = function(searchText) {
+    var cities = document.createElement("div");
+    cities.textContent = searchText;
+    cities.addEventListener("click", function () {
+        getCity(this.textContent)
+    })
+    searchHistoryList.append(cities);
+}
+
+searchBtn.addEventListener("click", function () {
+    var city = document.getElementById("searchResponse").value.trim()
+    if (city == "") {
+        alert("Provide city name!");
+    }
+    else {
+        searchHistory = searchHistory.filter(elem => city !== elem)
+        searchHistory.unshift(city);
+        searchHistory.splice(8);
+        localStorage.setItem("cities", JSON.stringify(searchHistory));
+        renderSearchHistory()
+    }
+})
+renderSearchHistory()
 
