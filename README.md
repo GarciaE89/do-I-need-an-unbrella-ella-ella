d5911478853d3e6c3a845df6bc7a3ed2cbcae32e
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-09-06 21:28:25 -0600

    finished code and added readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # do-I-need-an-unbrella-ella-ella
+
+## User Story 
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria 
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+
+
+
+![Scheduler] <img width="1080" alt="Screen Shot 2021-09-06 at 9 24 47 PM" src="https://user-images.githubusercontent.com/74579301/132279714-f07ba142-fc5d-457a-a260-5d9617cadeff.png">
--- assets/js/script.js
+++ assets/js/script.js
@@ -57,3 +57,83 @@ searchBtn.addEventListener("click", function () {
 })
 renderSearchHistory()
 
+var CurrentWeatherAPI = function(city) {
+    return "q=" + city + "&appid=" + openWeatherApi;
+}
+var ForecastAPI = function(coord) {
+    return "lat=" + coord.lat + "&lon=" + coord.lon + "&exclude=current,minutely,hourly,alerts&appid=" + openWeatherApi;
+}
+
+searchBtn.addEventListener("click", () => getCity(searchHistory[0]))
+
+function getCity(cityInput) {
+    $.ajax({
+        url: currentCityURL + CurrentWeatherAPI(cityInput),
+        method: "GET"
+    })
+        .then(function (response) {
+            $("#city").empty()
+            // console.log(response);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            console.log(lat)
+            console.log(lon)
+            var temp = (response.main.temp - 273.15) * 1.80 + 32;
+            $("#city").append(response.name + " (" + (now.format("l")) + ")");
+            $("#weather").html("<br />" + "Temperature: " + temp.toFixed(1) + "&#8457;" + "<br /><br />" + "Humidity: " + response.main.humidity + "%" + "<br /><br />" + "Windspeed: " + response.wind.speed + "mph" + "<br /><br />");
+
+            return $.ajax({
+                url: forecastURL + ForecastAPI(response.coord),
+                method: "GET"
+            })
+        })
+
+        .then(function (response) {
+            $("#ultaVioletIndex").empty()
+            var uvi = response.daily[0].uvi
+            uvi.className = "ultaVioletIndex"
+            var uviBtn = document.createElement("button");
+            uviBtn.className = "button btn btn-danger";
+            uviBtn.append(uvi)
+            $("#ultaVioletIndex").append("UV Index: ")
+            $("#ultaVioletIndex").append(uviBtn);
+
+            var forecast = document.getElementById("forecast")
+            var forecastHeader = document.getElementById("forecastHeader")
+            forecastHeader.innerHTML = "5-Day Forecast:" + "<br />";
+            $("#forecast").empty()
+            $("#forecastHeader").empty()
+
+            for (let a = 1; a < 6; a++) {
+                var createForecastDiv = function(a) {
+                    var forecastDiv = document.createElement("div");
+                    forecastDiv.className = "forecastDiv col-sm bg-primary mb-3 mr-3 p-4";
+
+                    var timestamp = response.daily[a].dt * 1000
+                    var date = document.createElement("div");
+                    date.className = "date";
+                    date.textContent = Intl.DateTimeFormat("en").format(timestamp)
+
+                    var imgDiv = document.createElement("div");
+                    imgDiv.className = "imgDiv";
+
+                    var icon = document.createElement("img");
+                    icon.setAttribute("id", "image");
+                    var conditions = response.daily[a].weather[0].icon
+                    icon.src = "http://openweathermap.org/img/wn/" + conditions + "@2x.png"
+
+                    var dailyStats = document.createElement("div");
+                    dailyStats.className = "div dailyStats";
+                    var dailyTemp = (response.daily[a].temp.day - 273.15) * 1.80 + 32;
+                    var dailyHumidity = (response.daily[a].humidity);
+                    dailyStats.innerHTML = "Temp: " + dailyTemp.toFixed(1) + " &#8457; " + "<br /><br />" + "Humidity: " + dailyHumidity + " % ";
+
+                    forecastDiv.append(date, icon, dailyStats)
+                    forecast.append(forecastDiv);
+                }
+                createForecastDiv(a)
+            }
+            console.log(response);
+        });
+}
+getCity(searchHistory[0])
